7b025790292c5c783baa8ebd78bf84db9cf82f86
jojupacr5 <joseph.paucar@gmail.com>
2024-10-29 22:24:01 -0500

    Estructurando los archivos para recibir las rutas del restweb, trabajando con el ejemplo de Todo

---
+++ src/presentation/todos/controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+export class TodosController {
+
+  //* Dependency Injection
+  constructor() {}
+
+  public getTodos =  (req: Request, res: Response) => {
+      
+    res.json([
+      { id: 1, text: 'Buy milk', createdAt: new Date() },
+      { id: 2, text: 'Buy bread', createdAt: null },
+      { id: 3, text: 'Buy butter', createdAt: new Date() }
+    ]);
+  }
+}
